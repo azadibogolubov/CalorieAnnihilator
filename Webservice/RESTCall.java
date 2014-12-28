c8a1dfc999f9378a90931154484fb7cee0095d84
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2014-12-27 18:34:11 -0800

    Modified REST call to use name search

--- Webservice/RESTCall.java
+++ Webservice/RESTCall.java
@@ -29,33 +29,36 @@ public class RESTCall
     return sb.toString();
   }
 
-  public static Object getFoodName(Object input)
+  public static Object getFoodName(Object input, int num)
   {
-      Object reportData = ((JSONObject) input).get("report");
-      Object foodsData = ((JSONObject) reportData).get("foods");
-      Object foodsArray = ((JSONArray) foodsData).get(0);
+      Object reportData = ((JSONObject) input).get("list");
+      Object max = ((JSONObject) reportData).get("end");
+      Object foodsData = ((JSONObject) reportData).get("item");
+      Object foodsArray = ((JSONArray) foodsData).get(num);
       Object foodName = ((JSONObject) foodsArray).get("name");
       return foodName;
   }
 
   public static Object getCalorieCount(Object input)
   {
-      Object reportData = ((JSONObject) input).get("report");
+      /*Object reportData = ((JSONObject) input).get("report");
       Object foodsData = ((JSONObject) reportData).get("foods");
       Object foodsArray = ((JSONArray) foodsData).get(0);
       Object nutrientData = ((JSONObject) foodsArray).get("nutrients");
       Object nutrientArray = ((JSONArray) nutrientData).get(0);
       Object calorieCount = ((JSONObject) nutrientArray).get("value");
-      return calorieCount;
+      return calorieCount;*/
+      return 0;
   }
 
   public static Object getServingSize(Object input)
   {
-      Object reportData = ((JSONObject) input).get("report");
+      /*Object reportData = ((JSONObject) input).get("report");
       Object foodsData = ((JSONObject) reportData).get("foods");
       Object foodsArray = ((JSONArray) foodsData).get(0);
       Object servingSize = ((JSONObject) foodsArray).get("measure");
-      return servingSize;
+      return servingSize;*/
+      return 0;
   }
 
 	public static void main(String[] args)
@@ -63,15 +66,17 @@ public class RESTCall
     JSONParser parser = new JSONParser();
     try 
     {
-      String s = issueGetRequest("http://api.data.gov/usda/ndb/nutrients/?format=json&api_key=3hVnhFvj1VAagD29p9Q5b5MeYenARhmAvyX2suCf&nutrients=208&max=1");
+      String s = issueGetRequest("http://api.data.gov/usda/ndb/search/?format=json&q=cheese&max=25&offset=0&api_key=DEMO_KEY");
       Object parsedData = parser.parse(s);
-      Object servingSize = getServingSize(parsedData);
-      Object numCalories = getCalorieCount(parsedData);
-      Object foodName = getFoodName(parsedData);
-
-      System.out.println("Name: " + foodName);
-      System.out.println("Calories: " + numCalories);
-      System.out.println("Serving Size: " + servingSize);
+      //Object servingSize = getServingSize(parsedData);
+      //Object numCalories = getCalorieCount(parsedData);
+      for (int i = 0; i < 25; i++)
+      {
+	Object foodName = getFoodName(parsedData, i);
+	System.out.println("Name: " + foodName);
+      }
+      //System.out.println("Calories: " + numCalories);
+      //System.out.println("Serving Size: " + servingSize);
     }
     catch (ParseException e)
     {
