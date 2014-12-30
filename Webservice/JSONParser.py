badca948627b65f2ac4fc34d97053f90e9dfba80
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2014-12-29 19:32:24 -0800

    Added user search

--- Webservice/JSONParser.py
+++ Webservice/JSONParser.py
@@ -1,14 +1,17 @@
 import json, requests
-url = "http://api.data.gov/usda/ndb/search/?format=json&q=cheese&max=25&offset=0&api_key=DEMO_KEY"
+name = raw_input("Enter a food name: ")
+url = "http://api.data.gov/usda/ndb/search/?format=json&q=" + name + "&max=25&offset=0&api_key=3hVnhFvj1VAagD29p9Q5b5MeYenARhmAvyX2suCf"
 nameResponse = requests.get(url=url).json()
 
 for i in range(0, 5):
 	name = nameResponse["list"]["item"][i]["name"]
 	ndbno = nameResponse["list"]["item"][i]["ndbno"]
+
 	print("Name: " + name)
 	print("NDB #: " + ndbno)
 
 	url2 = "http://api.data.gov/usda/ndb/nutrients/?format=json&api_key=3hVnhFvj1VAagD29p9Q5b5MeYenARhmAvyX2suCf&nutrients=208&ndbno=" + ndbno + "&max=1"
 	calResponse = requests.get(url=url2).json()
+
 	print("Measure: " + calResponse["report"]["foods"][0]["measure"])
 	print("Calories: " + calResponse["report"]["foods"][0]["nutrients"][0]["value"] + '\n')
