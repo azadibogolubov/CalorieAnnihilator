d4b1bc88d29c4230d10b37831cc7f310ea37252a
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2015-01-01 17:16:05 -0800

    Added auxillary functions for JSON handling

--- Android Code/src/com/tutorazadi/CalorieAnnihilator/AvoidanceActivity.java
+++ Android Code/src/com/tutorazadi/CalorieAnnihilator/AvoidanceActivity.java
@@ -11,21 +11,21 @@ import android.widget.Button;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import java.util.*;
 
-public class AvoidanceActivity extends Activity
-{
-    private static String url = "http://api.data.gov/usda/ndb/search/?format=json&q=papa%20johns&max=25&offset=0&api_key=3hVnhFvj1VAagD29p9Q5b5MeYenARhmAvyX2suCf";
+public class AvoidanceActivity extends Activity {
+    private static String url;
     private ListView listView;
     private ArrayAdapter<String> adapter;
     private ArrayList<String> listItems;
+    private EditText searchTxt;
 
     JSONObject list = null;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.avoidance);
 
@@ -36,18 +36,35 @@ public class AvoidanceActivity extends Activity
         listView = (ListView) findViewById(R.id.listView);
         adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, listItems);
         listView.setAdapter(adapter);
+        searchTxt = (EditText) findViewById(R.id.searchTxt);
 
         Button searchBtn = (Button) findViewById(R.id.searchBtn);
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                handleJSON();
+            public void onClick(View v)
+            {
+                String foodName = searchTxt.getText().toString();
+                handleJSON(foodName);
             }
         });
     }
 
-    public void handleJSON()
+    public void handleJSON(String foodName)
     {
+        String name = handleNameJSON(foodName);
+        String nutrition = handleNutritionJSON();
+        listItems.add(name + "\n" + nutrition);
+        adapter.notifyDataSetChanged();
+    }
+
+    public String handleNutritionJSON()
+    {
+        return "";
+    }
+
+    public String handleNameJSON(String foodName)
+    {
+        url = "http://api.data.gov/usda/ndb/search/?format=json&q=papa%20johns&max=25&offset=0&api_key=3hVnhFvj1VAagD29p9Q5b5MeYenARhmAvyX2suCf";
         JSONParser jParser = new JSONParser();
         JSONObject json = jParser.getJSONFromUrl(url);
         try
@@ -59,13 +76,13 @@ public class AvoidanceActivity extends Activity
             listItems.add(nameObj);
             name = item.getJSONObject(1);
             nameObj = name.getString("name");
-            listItems.add(nameObj);
-            adapter.notifyDataSetChanged();
+            return nameObj;
         }
         catch (JSONException e)
         {
             e.printStackTrace();
         }
+        return "";
     }
 
 }
