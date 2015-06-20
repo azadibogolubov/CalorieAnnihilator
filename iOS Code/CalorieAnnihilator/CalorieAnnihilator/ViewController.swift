bb700402b64e1892b2acc03b4db511e9118b67e0
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2015-06-19 20:38:53 -0700

    Updated references to Swift Swift 1.2 and commented code

--- iOS Code/CalorieAnnihilator/CalorieAnnihilator/ViewController.swift
+++ iOS Code/CalorieAnnihilator/CalorieAnnihilator/ViewController.swift
@@ -18,18 +18,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // The query to make to receive the JSON information from the USDA database.
         let url = NSURL(string: "http://api.data.gov/usda/ndb/nutrients/?format=json&api_key=3hVnhFvj1VAagD29p9Q5b5MeYenARhmAvyX2suCf&nutrients=208&ndbno=01218&max=1500")!
         let urlSession = NSURLSession.sharedSession()
         
+        // Build the JSON query.
         let jsonQuery = urlSession.dataTaskWithURL(url, completionHandler: { data, response, error -> Void in
             if (error != nil) {
                 println(error.localizedDescription)
             }
             var err: NSError?
             
-            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as! NSDictionary
             
+            // Parse all of the JSON values.
             if let report = jsonResult["report"] as? NSDictionary {
                 if let foods = report["foods"] as? NSArray {
                     if let firstName = foods[0] as? NSDictionary {
